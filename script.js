edaf1187529c974b893cc5298c1bf480562341c6
SURYA-200346 <surya.jsdev@gmail.com>
2025-11-29 00:19:10 +0530

    added some content by surya-softdev

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const finder = document.getElementById("Finder");
 const output = document.getElementById("output");
-const API_KEY = "aeeb4bde";
 
 function getUserInput() {
     const value = document.getElementById("movifinder").value.trim();
@@ -11,29 +10,30 @@ function getUserInput() {
     return value;
 }
 
-finder.addEventListener("click", async function findmovie() {
+finder.addEventListener("click", async function () {
     const movie = getUserInput();
     if (!movie) return;
-
-    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${movie}`;
-
+    const url = `https://www.omdbapi.com/?apikey=aeeb4bde&t=${movie}&plot=short`;
     try {
         const res = await fetch(url);
         const data = await res.json();
-
         if (data.Response === "False") {
             output.innerHTML = `<p>Movie not found</p>`;
             return;
         }
-
         output.innerHTML = `
-            <img src="${data.Poster}" alt="${data.Title}">
-       <strong>Movie Name:</strong><span>${data.Title}</span>
-            <p>${data.Year}</p>
-            <P>${data.Language}</p>
-            <P>${data.imdbRating}</p>
+        <div class="movieresult">
+        <img src="${data.Poster}" alt="${data.Title}">
+        <p><strong>Movie Name:</strong>${data.Title}</p>
+        <p> <strong>imdb:</strong> ${data.Actors}</p>
+        <p> <strong>Realsed Year:</strong>${data.Released}</p>
+        <p> <strong>language:</strong>${data.Language}</p>
+        <p> <strong>imdb:</strong> ⭐ ${data.imdbRating}</p>
+            </div>
         `;
+
     } catch (error) {
         alert("Failed to fetch");
     }
 });
+
